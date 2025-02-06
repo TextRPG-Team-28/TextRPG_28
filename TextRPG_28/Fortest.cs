2a580182698c74313c35b760660c98f3324012df
JuyeonOdosn <wbsksodkqh@naver.com>
2025-02-06 19:45:41 +0900

    [test] 작업을 어디서 하고 있는지 확인용

--- TextRPG_28/Fortest.cs
+++ TextRPG_28/Fortest.cs
@@ -8,7 +8,41 @@ namespace TextRPG_28
 {
     internal class Fortest
     {
-        string test = "테스트중";
-        
+        public interface ICharacter
+        {
+            string Name { get; set; }
+            int Level { get; set; }
+            int Attack { get; set; }
+            int Health { get; set; }
+            int Defend { get; set; }
+            int Gold { get; set; }
+
+        }
+
+        public class Warrior : ICharacter
+        {
+            public string Name { get; set; }
+            public int Level { get; set; }
+            public int Attack { get; set; }
+            public int Health { get; set; }
+            public int Defend { get; set; }
+            public int Gold { get; set; }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Warrior warrior = new Warrior();
+            {
+                warrior.Attack = 10;
+                warrior.Health = 100;
+                warrior.Defend = 5;
+                warrior.Gold = 1500;
+                warrior.Level = 1;
+            }
+        }
+
     }
 }
