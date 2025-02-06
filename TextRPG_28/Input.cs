46e7c857574a2512bf5111f27736015b76f653a8
daeng98 <daeng9821@gmail.com>
2025-02-06 20:46:57 +0900

    1번 2번

---
+++ TextRPG_28/Input.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_28
+{
+    static class Input          // 키입력 관련 클래스
+    {
+        public static int GetInput(int min, int max)            // min, max의 값 이내의 숫자 입력시 input을 return 반복문 탈출. 그 외에는 잘못 입력됫다는 텍스트 출력 후 반복
+        {
+            while (true)
+            {
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+
+                if (int.TryParse(Console.ReadLine(), out int input) && (input >= min) && (input <= max))
+                {
+                    Console.WriteLine(input);
+                    return input;
+                }                 
+
+                Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TextRPG_28/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_28
+{
+    public class Player         // 플레이어 관련 클래스
+    {
+        public int Level { get; }
+        public string Name { get; }
+        public string Job { get; }
+        public int Attack { get; }
+        public int Defense { get; }
+        public int Gold { get; set; }
+        public int Hp { get; }
+        public int MaxHp { get; }
+
+        public Player(int level, string name, int attak, int defense, int maxHp, int gold)          // 생성시 플레이어 초기 설정, 직업은 추가 예정이 아직 없기때문에 아직은 고정
+        {
+            Level = level;
+            Name = name;
+            Job = "전사";
+            Attack = attak;
+            Defense = defense;
+            Gold = gold;
+            Hp = maxHp;
+            MaxHp = maxHp;
+        }
+
+        public void PlayerInfo()            // 플레이어 정보 화면 메서드
+        {
+            Console.WriteLine($"Lv. {Level.ToString("00")}");
+            Console.WriteLine($"{Name} ( {Job} )");
+            Console.WriteLine($"공격력 : {Attack}");
+            Console.WriteLine($"방어력 : {Defense}");
+            Console.WriteLine($"체력 : {Hp} / {MaxHp}");
+            Console.WriteLine($"Gold : {Gold} gold");
+        }
+    }
+}
