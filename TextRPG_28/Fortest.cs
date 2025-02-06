81afa618222d47dbcdb1049f9da1988d092910f2
JuyeonOdosn <wbsksodkqh@naver.com>
2025-02-06 19:28:25 +0900

    test용

---
+++ TextRPG_28/Fortest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_28
+{
+    internal class Fortest
+    {
+        string test = "테스트중";
+        
+    }
+}
