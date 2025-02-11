3b39de04228a9617573347c5f16985fe79dc915a
gggji1211 <c061211@naver.com>
2025-02-11 09:16:39 +0900

    Result추가

--- TextRPG_28/Result.cs
+++ TextRPG_28/Result.cs
@@ -1,47 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TextRPG_28
 {
     internal class Result
     {
-        public static void  ShowBattleResult(Character.Player warrior, List<Character.Monster> monsters)
+        public static void ShowBattleResult(Player player, List<Monster> monsters) //플레이어 몬스터 받아오기
         {
             Console.Clear();
             Console.WriteLine("Battle!! - Result\n");
 
-            if (warrior.Health <= 0)
+            if (player.Hp <= 0) // hp가 0 이면 
             {
-                Console.WriteLine("You Lose\n");
+                Console.WriteLine("짐...");
+                Console.WriteLine("당신은 던전에서 패배했습니다.");
+                Console.WriteLine("\n0. 게임 종료\n>>");
+                Console.ReadLine(); // 사용자가 종료하도록 대기
+                Environment.Exit(0); // 프로그램 종료
             }
             else
             {
-                Console.WriteLine("Victory\n");
-                Console.WriteLine($"던전에서 {monsters.Count}마리의 몬스터를 잡았습니다.");
-                Console.WriteLine("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
-                Console.WriteLine("⣿⣿⣿⣿⣿⣿⡟⠏⠇⠇⠇⠫⡛⣿⣿⣿⣿⣿⣿");
-                Console.WriteLine("⣿⣿⣿⣿⣿⣿⣮⠐⠌⢌⠢⠳⢶⣿⣿⣿⣿⣿⣿");
-                Console.WriteLine("⣿⣿⣿⣿⣿⣿⠡⠡⠡⠡⠡⠡⡑⢹⣿⣿⣿⣿⣿");
-                Console.WriteLine("⣿⣿⡿⣛⢙⢛⠡⠡⡹⣥⢡⣡⡑⡸⡿⣿⣿⣿⣿");
-                Console.WriteLine("⣿⣿⣿⡬⠨⣴⣌⡂⢂⠪⢟⠯⠇⣤⣷⢸⣿⣿⣿");
-                Console.WriteLine("⣿⣿⣿⣿⣿⣿⣿⢇⢇⣧⣇⡕⢼⣿⣿⣿⣿⣿⣿");
-                Console.WriteLine("⣿⣿⣿⣿⣿⣿⣿⣸⣪⡯⢗⣛⣾⣿⣿⣿⣿⣿⣿");
-                Console.WriteLine("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+                Console.WriteLine("이김!!"); //승리
+                int Monsterscount = monsters.Count(monster => monster.isDead);
+                Console.WriteLine($"던전에서 {Monsterscount}마리의 몬스터를 처치했습니다.");
 
+                
             }
-
-            Console.WriteLine($"\nLv.{warrior.Level} {warrior.Name}");
-            Console.WriteLine($"HP {Math.Max(0, warrior.Health)}\n");
+            Console.WriteLine($"\nLv.{player.Level} {player.Name}");
+            Console.WriteLine($"HP {Math.Max(0, player.Hp)}\n");
             Console.WriteLine("0. 다음\n>>");
             Console.ReadLine(); // 사용자가 다음으로 진행하도록 대기
         }
 
-        static public bool CheckBattleEnd(Character.Player warrior, List<Character.Monster> monsters)
+        public static bool CheckBattleEnd(Player player, List<Monster> monsters)
         {
-            return warrior.Health <= 0 || monsters.Count == 0;
+            if (player.Hp <= 0 || monsters.All(m => m.isDead))
+            {
+                ShowBattleResult(player, monsters); // 전투 종료 후 결과 화면 호출
+                return true;
+            }
+            return false;
         }
     }
 }
