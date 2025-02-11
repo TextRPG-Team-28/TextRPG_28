f72cecad997514bf4117244d2728a88ddccb654e
JuyeonOdosn <wbsksodkqh@naver.com>
2025-02-11 14:45:02 +0900

    [chore] Player 클래스에서 마나 추가
    ㅈㄱㄴ

--- TextRPG_28/Player.cs
+++ TextRPG_28/Player.cs
@@ -17,6 +17,9 @@ namespace TextRPG_28
         public int Gold { get; set; }
         public int Hp { get; set; }
         public int MaxHp { get; }
+
+        public int Mp { get; } // 마나 추가
+        public int MaxMp { get; } // 마나마나
         public bool isDead { get; set; }
 
         public Player(string name)
@@ -24,7 +27,7 @@ namespace TextRPG_28
             Name = name;
         }
 
-        public Player(int level, string name, string job, int attak, int defense, int maxHp, int gold, bool dead)          // 생성시 플레이어 초기 설정, 직업은 추가 예정이 아직 없기때문에 아직은 고정
+        public Player(int level, string name, string job, int attak, int defense, int maxHp, int gold, int mp, int maxMp, bool dead)          // 생성시 플레이어 초기 설정, 직업은 추가 예정이 아직 없기때문에 아직은 고정
         {
             Level = level;
             Name = name;
@@ -34,6 +37,8 @@ namespace TextRPG_28
             Gold = gold;
             Hp = maxHp;
             MaxHp = maxHp;
+            Mp = mp; // 스킬구현에 필요한 마나 추가된 부분
+            MaxMp = maxMp; // 위와 동일
             isDead = dead;
         }
 
@@ -48,6 +53,7 @@ namespace TextRPG_28
             Console.WriteLine($"공격력 : {Attack}");
             Console.WriteLine($"방어력 : {Defense}");
             Console.WriteLine($"체력 : {Hp} / {MaxHp}");
+            Console.WriteLine($"MP : {Mp} / {MaxMp}"); // 스킬 구현 마나 추가된 부분
             Console.WriteLine($"Gold : {Gold} gold");
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
